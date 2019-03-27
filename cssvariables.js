9f54fa9adca84de78a6d27b89fd6834e3d8732e6
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-03-27 15:00:08 +0530

    HTML and CSS updated

---
+++ cssvariables.js
@@ -0,0 +1,13 @@
+function evalSlider()
+{ var slider_Spacing=document.getElementById('spacing').value;
+var slider_Blur=document.getElementById('blur').value;
+var slider_Color=document.getElementById('baseColor').value;
+var img=document.getElementById('img');
+var spacingContainer=
+console.log(slider_Spacing);
+console.log(slider_Blur);
+console.log(slider_Color);
+if (slider_Spacing)
+{
+    spacing
+}
